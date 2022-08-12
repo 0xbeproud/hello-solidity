4f54ccc10fb92a34b84a37446275af3dafe000c4
logan-beproud <logan.beproud@krosslab.io>
2022-08-12 16:32:48 +0900

    CollectibleGenerator, CollectibleMinter 추가

--- contracts/oftensee/CollectibleGenerator.sol
+++ contracts/oftensee/CollectibleGenerator.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract CollectibleGenerator {}
+import "./Collectible.sol";
+
+contract CollectibleGenerator {
+    function generate(string memory name, string memory symbol) {
+        Collectible collectible = new Collectible(name, symbol);
+    }
+}
