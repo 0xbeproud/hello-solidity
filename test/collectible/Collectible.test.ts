809c81a78653b77d4b050a8b05332f493ccf9a70
logan-beproud <logan.beproud@krosslab.io>
2022-08-12 17:20:52 +0900

    Update Collectible.test.ts

--- test/collectible/Collectible.test.ts
+++ test/collectible/Collectible.test.ts
@@ -6,6 +6,7 @@ import {expect} from 'chai';
 describe('Collectible', () => {
     const name: string = 'hello contract';
     const symbol: string = 'COLECTIBLE';
+    const tokenURI: string = 'tokenURL';
 
     let sut: Collectible;
     let owner: SignerWithAddress, user: SignerWithAddress;
@@ -20,9 +21,48 @@ describe('Collectible', () => {
 
     describe('mint', () => {
         it('ok', async () => {
-            let tx = await sut.safeMint(user.address, 'hello');
+            let tx = await sut.safeMint(user.address, tokenURI);
             await tx.wait();
             await expect(await sut.balanceOf(user.address)).to.equals(1);
+        });
+    });
+
+    describe('name', () => {
+        it('ok', async () => {
+            await expect(await sut.name()).to.equals(name);
+        });
+    });
+
+    describe('symbol', () => {
+        it('ok', async () => {
+            await expect(await sut.symbol()).to.equals(symbol);
+        });
+    });
+
+    describe('ownerOf', () => {
+        it('ok', async () => {
+            let tx = await sut.safeMint(user.address, tokenURI);
+            await tx.wait();
+            await expect(await sut.ownerOf(0)).to.equals(user.address);
+        });
+    });
+
+    describe('balanceOf', () => {
+        it('ok', async () => {
+            await expect(await sut.balanceOf(user.address)).to.equals(0);
+            let tx = await sut.safeMint(user.address, tokenURI);
+            await tx.wait();
+            tx = await sut.safeMint(user.address, tokenURI);
+            await tx.wait();
+            await expect(await sut.balanceOf(user.address)).to.equals(2);
+        });
+    });
+
+    describe('tokenURI', () => {
+        it('ok', async () => {
+            let tx = await sut.safeMint(user.address, tokenURI);
+            await tx.wait();
+            await expect(await sut.tokenURI(0)).to.equals('https://example.com/nft/tokenURL');
         });
     });
 });
