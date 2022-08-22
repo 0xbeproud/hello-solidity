f04a3ef76dc8400f29c81f1525c65f8ea1fb1dc9
be-proud <betheproud@gmail.com>
2022-08-22 22:26:07 +0900

    Collectible 변경

--- contracts/collectible/Collectible.sol
+++ contracts/collectible/Collectible.sol
@@ -3,17 +3,24 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Collectible is ERC721, ERC721URIStorage, Ownable {
+contract Collectible is ERC721, ERC721URIStorage, ERC721Enumerable, ERC721Burnable, ERC721Pausable, Ownable {
     using Counters for Counters.Counter;
+
     Counters.Counter private _tokenIdCounter;
+    string private _baseURI;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://example.com/nft/";
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseURI
+    ) ERC721(name, symbol) {
+        _baseURI = baseURI;
     }
 
     function safeMint(address to, string memory uri) public onlyOwner {
@@ -23,13 +30,33 @@ contract Collectible is ERC721, ERC721URIStorage, Ownable {
         _setTokenURI(tokenId, uri);
     }
 
-    // The following functions are overrides required by Solidity.
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURI;
+    }
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    virtual
+    override(ERC721, ERC721Enumerable)
+    returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
